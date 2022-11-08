635789723f5fd6348e042ed4754f9336fe84969b
shayenarno01 <20209189@itla.edu.do>
2022-11-08 00:02:55 -0400

    ..

--- src/app/home/UserService/user-service.service.ts
+++ src/app/home/UserService/user-service.service.ts
@@ -1,25 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const cors = require('cors')
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
+  headers = new HttpHeaders();
 
-  constructor(private http: HttpClient, private fb: FormBuilder) {}
+  constructor(private http: HttpClient, private fb: FormBuilder) {
 
-  private BaseURI = 'https://localhost:7133/api';
+    this.headers.append("Content-type", "aplicaction/json");
+    this.headers.getAll("Access-control-Allow-Origen: *");
+    this.headers.getAll("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");  
+    this.headers.getAll("Access-Control-Allow-Headers: Content-type, X-Auth-Token, Origin, Authorization");  
+
+  }
+
+  private BaseURI = 'https://localhost:7133/api/Usuarios/login';
   private Base1 = '/Usuarios/login';
   private Base2 = 'http://localhost:54277/api';
 
   formModel = this.fb.group({
     Email: ['', Validators.email],
-    Clave: this.fb.group({
-      Clave: ['', [Validators.required, Validators.minLength(4)]],
-      ConfirmarClave: ['', Validators.required]
-    }), /*{ validator: this.comparePasswords }),*/
+    Clave: ['', [Validators.required]],
+      ConfirmarClave: ['', Validators.required],
+    // Clave: this.fb.group({
+      
+    // }), /*{ validator: this.comparePasswords }),*/
     Usuario1: [''],
     Telefono: ['', Validators.required],
     Rol: [''],
@@ -27,7 +38,7 @@ export class UserServiceService {
   });
 
   login(formData: any) {
-    return this.http.post(this.BaseURI + '/Usuarios/login', formData);
+    return this.http.post(this.BaseURI, formData);
   }
 
 
