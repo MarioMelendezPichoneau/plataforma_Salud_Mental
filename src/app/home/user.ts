4b24cd61c24f76484602a88057d00d156c27c73b
MarioMelendezPichoneau <20197959@itla.edu.do>
2022-12-14 01:48:04 -0400

    login funciona

---
+++ src/app/home/user.ts
@@ -0,0 +1,10 @@
+export interface user{
+
+    email: String;
+    clave: String;
+    confirmarClave: String;
+    Usuario1: String;
+    Telefono: String;
+    Rol: String;
+    Status: String;
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserServiceService } from '../home/UserService/user-service.service';
+import { user} from "../home/user";
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 
 @Component({
@@ -12,34 +14,59 @@ import { UserServiceService } from '../home/UserService/user-service.service';
 export class LoginComponent implements OnInit {
 
   formModel = {
-    Email: '',
-    Clave: ''
+    email: '',
+    clave: ''
   }
 
+  
 
-  constructor(public service: UserServiceService, private router: Router) { 
+
+  constructor(private http: HttpClient, private router: Router) { 
     
   }
+
 
   ngOnInit(): void {
     if (localStorage.getItem('token') != null)
       this.router.navigateByUrl('/homeInside');
   }
 
-  onSubmit(form: NgForm) {
-    this.service.login(form.value).subscribe(
-      (res: any) => {
-        localStorage.setItem('token', res.token);
-        this.router.navigateByUrl('/home');
-      },
-      err => {
-        if (err.status == 400)
-          console.error('Incorrect username or password.', 'Authentication failed.');
-        else
-          console.log(err);
-      }
-    );
+  // onSubmit(form: NgForm) {
+  //   this.http.post("https://localhost:7133/api/Usuarios/login",form.value).subscribe(
+  //     (res: any) => {
+  //       localStorage.setItem('token', res.token);
+  //       this.router.navigateByUrl('/home');
+  //     },
+  //     err => {
+  //       if (err.status == 400)
+  //         console.error('Incorrect username or password.', 'Authentication failed.');
+  //       else
+  //         console.log(err);
+  //     }
+  //   );
   
+  // }
+
+
+
+  login() {
+    const user = {email: this.formModel.email, clave: this.formModel.clave};
+    this.http.post("https://localhost:7133/api/Usuarios/login", user).subscribe( (res: any) => {
+      localStorage.setItem('token', res.token);
+      this.router.navigateByUrl("/Inicio_Inside");
+    },
+    err => {
+      if (err.status == 400)
+        console.error('Incorrect username or password.', 'Authentication failed.');
+      else
+        console.log(err);
+    });
   }
 
+
+
+
+
+
+  
 }
