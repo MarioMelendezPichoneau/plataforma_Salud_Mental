13eaacb39f7916ad754aa4f68fd510b968e1c3a7
shayenarno01 <103524327+shayenarno01@users.noreply.github.com>
2022-12-14 01:45:02 -0400

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
   title = 'carousel';
 
+  
   images = [
     {
       imageSrc:
@@ -30,7 +32,18 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"2b934abc8d00ce8c779aa4463ab50e238","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      (window as any).kommunicate = m; m._globals = kommunicateSettings;
+  })(document, (window as any).kommunicate|| {});
+}
   }
 
-}
+
+
+
