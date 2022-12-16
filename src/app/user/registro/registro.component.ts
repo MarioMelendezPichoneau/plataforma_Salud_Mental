b9b7ba3d5ff9c61a26e784e2a8486e184ab023eb
MarioMelendezPichoneau <20197959@itla.edu.do>
2022-12-15 21:39:26 -0400

    registro funcionando

--- src/app/user/registro/registro.component.ts
+++ src/app/user/registro/registro.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formModel = {
+    email: '',
+    clave: '',
+    usuario1: '',
+    telefono: '',
+    rol: '1',
+    status: 'A'
+  }
+
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  registro() {
+    const user = {
+      email: this.formModel.email, 
+      clave: this.formModel.clave, 
+      usuario: this.formModel.usuario1, 
+      telefono: this.formModel.telefono,
+      rol: this.formModel.rol,
+      status: this.formModel.status
+    };
+    this.http.post("https://localhost:7133/api/Usuarios/Registro", user).subscribe( (res: any) => {
+      localStorage.setItem('token', res.token);
+      alert("Usuario Registrado");
+      this.router.navigateByUrl("/Inicio_Inside");
+    },
+    err => {
+      if (err.status == 400)
+        console.error(err);
+      else
+        console.log(err);
+    });
+  }
+
 
 }
