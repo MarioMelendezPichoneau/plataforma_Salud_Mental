47d9cf849c087f58445e1de0968fb47b21433536
shayenarno01 <103524327+shayenarno01@users.noreply.github.com>
2022-12-16 13:51:07 -0400

    .

--- src/app/inicio-inside/inicio-inside.component.ts
+++ src/app/inicio-inside/inicio-inside.component.ts
@@ -12,7 +12,15 @@ export class InicioInsideComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"35306386cf03ced0db82ac7dc8dc9c5e3","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
   }
 
 }
